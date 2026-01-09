3823f6258c9333d9ab5b31f1795cae54538839a1
Sophia Williams <117466337+smjw@users.noreply.github.com>
2026-01-09 17:37:18 +0000

    enemy complete

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,8 +3,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("died")
-	
+	body.get_node("CollisionShape2D").queue_free()
+	Engine.time_scale = 0.5
 	timer.start()
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+	Engine.time_scale = 1
